e3b67cae6cdb6e1d0458c5c00b7f5ce5dd76f173
Thibault <th.jullion@gmail.com>
2016-02-03 11:58:54 +0100

    Ajax tirage en cours

--- public/js/connexion_inscription/script.js
+++ public/js/connexion_inscription/script.js
@@ -5,4 +5,7 @@
 $(".color_in").click( function(){
     var color_code = $(this).data("color");
     $(".color_player").val(color_code);
+
+    $(".color_in").removeClass("active");
+    $(this).addClass("active");
 });
